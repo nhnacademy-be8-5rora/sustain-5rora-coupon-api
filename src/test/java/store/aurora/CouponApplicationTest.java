51929bbff1fb4411ab899e8ee026c25e831b4154
adffdfewqefw <bbeekhw@gmail.com>
2025-01-23 15:32:55 +0900

    쿠폰 테스트 코드 추가

--- src/test/java/store/aurora/CouponApplicationTest.java
+++ src/test/java/store/aurora/CouponApplicationTest.java
@@ -1,17 +1,14 @@
 package store.aurora;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)  // JUnit 5 extension
-@SpringBootTest(classes = CouponApplication.class)
+
+@SpringBootTest
 class CouponApplicationTest {
 
     @Test
     void contextLoads() {
-        // 애플리케이션 컨텍스트가 잘 로드되는지 확인하는 테스트
-        // 특별한 검증 로직 없이 context가 로드되면 성공
+        // 이 테스트는 애플리케이션 컨텍스트가 정상적으로 로드되는지 확인합니다.
     }
 }
